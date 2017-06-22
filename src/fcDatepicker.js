5a8c3d31807297031a0e4e7f3a22172a519ee779
Zee <hao411284402@qq.com>
2017-06-22 10:40:20 +0800

    基本布局

--- src/fcDatepicker.js
+++ src/fcDatepicker.js
@@ -1,3 +1,25 @@
-/**
- * Created by Lawrence.li on 2017/6/22.
- */
+;(function ($) {
+    var fcDate = {}
+    fcDate.extendsOption = function (parms) {//合并参数
+        var $this = $(this);
+        var setting = $(this).data('faDatepicker');
+        if (typeof (setting) == 'undefined') {
+            var defaults = {
+
+            }
+            setting = $.extend({}, defaults, parms);
+            $(this).data('cusmodal', setting);
+        } else {
+            setting = $.extend({}, setting, parms);
+        }
+        return setting;
+    },
+    fcDate.init = function (parms) {//初始化
+        return this.each(function () {
+            var setting = fcDate.extendsOption(parms);
+        });
+    }
+
+    
+
+})(jQuery);
